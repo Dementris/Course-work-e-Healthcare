761f35be57d3fee402230fa0d57a310f04127c92
Dementris <bdlap.shl@gmail.com>
2023-05-03 20:25:14 +0300

    initial comit

---
+++ src/main/java/com/cursework/ehelthcare/CurseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.cursework.ehelthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurseWorkApplication.class, args);
+    }
+
+}
