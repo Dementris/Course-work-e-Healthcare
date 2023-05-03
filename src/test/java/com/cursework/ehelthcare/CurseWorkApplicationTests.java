6eca579bd3f7883fbe1de28cd6432d3eea3fa87c
Dementris <bdlap.shl@gmail.com>
2023-05-03 20:28:36 +0300

    initial comit

---
+++ src/test/java/com/cursework/ehelthcare/CurseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursework.ehelthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
