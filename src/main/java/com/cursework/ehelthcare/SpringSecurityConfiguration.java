a41df057a93097e28dd05c6034362ebd35ae7aa4
Dementris <bdlap.shl@gmail.com>
2023-05-05 15:27:21 +0300

    fix

--- src/main/java/com/cursework/ehelthcare/SpringSecurityConfiguration.java
+++ src/main/java/com/cursework/ehelthcare/SpringSecurityConfiguration.java
@@ -14,7 +14,7 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().requestMatchers("/","/static/css/**").permitAll()
+        http.authorizeHttpRequests().requestMatchers("/","/css/**","/img/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
