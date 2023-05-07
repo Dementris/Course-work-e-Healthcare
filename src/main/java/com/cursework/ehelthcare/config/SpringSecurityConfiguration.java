564e3a81e3db50548cba230fee80b73ebf82b0a2
Dementris <bdlap.shl@gmail.com>
2023-05-07 10:09:29 +0300

    update pom.xml

--- src/main/java/com/cursework/ehelthcare/config/SpringSecurityConfiguration.java
+++ src/main/java/com/cursework/ehelthcare/config/SpringSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.cursework.ehelthcare;
+package com.cursework.ehelthcare.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
